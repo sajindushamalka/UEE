50ae235fe5da45954018336bea1320644a5195ba
sithanga rashmika <87372439+IT21041716@users.noreply.github.com>
2023-10-20 02:38:27 +0530

    some pages done

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,104 +1,12 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
-// import NutritionPlanSelect from './Screen/NutritionPlanSelect';
-
-// export default function App() {
-//   return (
-//     <SafeAreaView>
-//     <View style={{backgroundColor:'black', height:'auto',paddingTop:50, padding:10}}>
-//       <StatusBar backgroundColor='orange'/>
-//      <NutritionPlanSelect />
-//     <Text style={{color:'white'}}>BBBB</Text>
-//     </View>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: 'black',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// import {createStackNavigator} from '@react-navigation/stack';
-// import {NavigationContainer} from '@react-navigation/native';
-// import Footer from './Screen/Footer';
-// import NutritionPlanSelect from './Screen/NutritionPlanSelect';
-// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
-// const Stack = createStackNavigator();
-// const Tab = createBottomTabNavigator();
-
-// function App() {
-//   return (
-//     <Stack.Navigator>
-      
-//       <Stack.Screen name='NutritionPlanSelect' component={NutritionPlanSelect} options={{headerShown:false}}/>
-//       {/* <Stack.Screen name='Footer' component={Footer} options={{headerShown:false}}/> */}
-      
-//     </Stack.Navigator>
-    
-//   )
-// }
-
-// export default () => {
-//   return (
-//     <NavigationContainer>
-//       <App/>
-//       <Tab.Navigator>
-//       <Tab.Screen name="Home" component={NutritionPlanSelect} />
-//       <Tab.Screen name="Settings" component={NutritionPlanSelect} />
-//     </Tab.Navigator>
-//     </NavigationContainer>
-//   )
-// }
-
-// import React from 'react';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-
-// import Footer from './Screen/Footer';
-// import NutritionPlanSelect from './Screen/NutritionPlanSelect';
-
-// const Stack = createStackNavigator();
-// const Tab = createBottomTabNavigator();
-
-// // Create a Stack Navigator for your main screens
-// function MainStack() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen name='NutritionPlanSelect' component={NutritionPlanSelect} options={{ headerShown: false }} />
-//       {/* Add more screens here */}
-//     </Stack.Navigator>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       {/* Use the MainStack as the default navigator */}
-//       <Tab.Navigator>
-//         <Tab.Screen name="Home" component={MainStack} options={{headerShown:false}}/>
-//         {/* Add more tabs here */}
-//         <Tab.Screen name="Settings" component={MainStack} options={{headerShown:false}}/>
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { View } from 'react-native';
+
 
 import Footer from './Screen/Footer';
 import NutritionPlanSelect from './Screen/NutritionPlanSelect';
@@ -106,6 +14,17 @@ import MealPlan from './Screen/MeakPlan';
 import MyMealPlan from './Screen/MyMealPlan';
 import MonthlyMealPlan from './Screen/monthlyMealPlan';
 import TreeMonthMealPlan from './Screen/treeMonthMealPlan';
+
+
+//sithanga
+import LandingPage from './Screen/IT21041716/componants/LandingPage ';
+import Login from './Screen/IT21041716/componants/userLogin'
+import Profile from './Screen/IT21041716/componants/profile'
+import Rewards from './Screen/IT21041716/componants/reward'
+import QR from './Screen/IT21041716/componants/qr'
+import Member from './Screen/IT21041716/componants/RenewMembership'
+import TimeTable from './Screen/IT21041716/componants/timeTable';
+import PaymentHistory from './Screen/IT21041716/componants/PaymentHistory';
 
 const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
@@ -118,37 +37,111 @@ function MainStack() {
       <Stack.Screen name='MonthlyMealPlan' component={MonthlyMealPlan} options={{ headerShown: false }} />
       <Stack.Screen name='TreeMonthMealPlan' component={TreeMonthMealPlan} options={{ headerShown: false }} />
       <Stack.Screen name='MyMealPlan' component={MyMealPlan} options={{ headerShown: false }} />
+      <Stack.Screen name='Rewards' component={Rewards} options={{ headerShown: false }} />
+      <Stack.Screen name='QR' component={QR} options={{ headerShown: false }} />
+      <Stack.Screen name='Member' component={Member} options={{ headerShown: false }} />
+      <Stack.Screen name='TimeTable' component={TimeTable} options={{ headerShown: false }} />
+      <Stack.Screen name='PaymentHistory' component={PaymentHistory} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
 
 function App() {
+  const [displayLogin, setDisplayLogin] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDisplayLogin(true);
+    }, 2000);
+  }, []);
+
+
   return (
-    <NavigationContainer> 
-      <Tab.Navigator barStyle={{backgroundColor:'orange'}}>
-        <Tab.Screen
-          name="Home"
-          component={MainStack}
-          options={{
-            tabBarIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'ios-home' : 'ios-home-outline'} size={size} color={color} />
-            ),
-            headerShown:false
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={MainStack}
-          options={{
-            tabBarIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'ios-settings' : 'ios-settings-outline'} size={size} color={color} />
-            ),
-            headerShown:false
-          }}
-        />
-      </Tab.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {!displayLogin ? (
+          <Stack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }} />
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Main"
+              component={MainScreen}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+
+function MainScreen() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: 'orange',
+        inactiveTintColor: '',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={MainStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Fontisto name="home" size={25} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={MainStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'ios-settings' : 'ios-settings-outline'} size={25} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Tutorial"
+        component={MainStack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="video-camera" size={25} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="photo"
+        component={MainStack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="photo" size={25} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'person-circle-outline' : 'person-circle-outline'} size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 export default App;
+
+
